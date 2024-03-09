bf87556e330be2b0aaf1f943bf2d6a9eb8d7d9e1
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-09 23:03:32 +0530

    added the homepage component

--- force-app/main/default/lwc/homePageComponent/homePageComponent.js
+++ force-app/main/default/lwc/homePageComponent/homePageComponent.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class HomePageComponent extends LightningElement {}
+export default class HomePageComponent extends NavigationMixin(LightningElement) {
+    handleClickButton() {
+        const pageReference = {
+            type: "standard__webPage",
+            attributes: {
+                url: "/add-record"
+            }
+        };
+        this[NavigationMixin.Navigate](pageReference);
+    }
+}
